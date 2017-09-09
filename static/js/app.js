328066a062a136fbbab452199af0b31ae963d147
Tsoyuzhu <z5113384@student.unsw.edu.au>
2017-09-09 21:51:09 +1000

    updated documentation and restructured scripts

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,12 @@
-	// Javascript code below is messy. The function which calls the others is found below and is called "dragHandler". It is called when a file is dropped onto the html div with class ".dropzone". 
 $(function() { 
-
-	var addEntry = function(data) {				// This function compiles the Handlebars template for the table of filenames. Called by Function "uploadSuccess".
-		var source = $("#tableTemplate").html(); 
-		var template = Handlebars.compile(source);	// Compile template data into a js function.
-		var context = template(data);
-		var renderedText = $(context);
-		$("#tableArea").empty();  
-		$("#tableArea").append(renderedText);		// Add the rendered table to the html.
-	};
-
-	var uploadSuccess = function(data) {			// This function is called when the flask application receives the "POST" request successfully. 
-		var dt = new Date();
-		var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();	// Create string for 24 hour time.
-		var object = {"time":time}			// These lines compile the Handlebars template for the time. 
-		var source = $("#timeTemplate").html(); 	// These four lines are Handlebars processes.
-		var template = Handlebars.compile(source);	// Compile template data into a js function.
-		var context = template(object);
-		var renderedText = $(context);
-		$("#time").empty();  
-		$("#time").append(renderedText);		// Add the rendered time to the html.
-		if (data == "unsuccessful_upload") {
-			$("#message").empty();			
-			$("#message").append("<p>Your last file upload failed because there is already an existing file with the same name.</p>");		
-			$("#message").append();				
-		}
-		else {
-			$("#message").empty();			
-			$("#message").append("<p>Your last file was uploaded successfully. </p>");	
-		}		
-		var promise = $.get("/filenames");
-		promise.then(addEntry);
-	};
-	
-	var uploadFailure = function(data) {			// This function will never be called since 
-	}; 							// request method is always "POST".
-
-	var dragHandler = function(event) {
-		event.preventDefault(); 			// Suppress default browser drag behaviour.
-	};
-
-	var dropHandler = function(event) {			// [THIS FUNCTION INVOKES ALL THE OTHERS]
-		event.preventDefault(); 			// Suppress default browser drop behaviour.
+	var dropHandler = function(event) {			// Function which saves a dropped file into the uploads folder.	
+		event.preventDefault(); 			
 		var files = event.originalEvent.dataTransfer.files; 
 
 		var formInput = new FormData();
 		formInput.append("inputFile", files[0]); 	// If there are multiple dragged files we accept only the first.
 	
-		var formRequest = {
+		var formRequest = {				// Form to post the file to the flask function.
 			url: "/",
 			method: "POST",
 			processData: false,
@@ -56,16 +15,54 @@ $(function() {
 		};
 		
 		var promise = $.ajax(formRequest); 		// Submit this data via ajax request.
-		promise.then(uploadSuccess,uploadFailure); 	// Callback since ajax is asynchronous.
+		promise.then(uploadSuccess,uploadFailure); 	// Callback function. 
 
 	};	
 
-	var dropHandlerSet = {					// Create set of handlers for drag and drop events. 
+	var dragHandler = function(event) {			// Function which allows files to be dragged into the page. 
+		event.preventDefault(); 			
+	};
+
+	var dropHandlerSet = {					// Define the handler functions for drag and drop events. 
 		dragover: dragHandler,
 		drop: dropHandler
 	};
 
-	$(".dropzone").on(dropHandlerSet); 			// Attach event handlers to events occuring on .dropzone
+	var uploadSuccess = function(data) {			// This function is called when the flask application receives the "POST" request successfully. 
+		var dt = new Date();				// var data contains the return value of the flask function index, "upload_success" or "upload_failure"
+		var time = dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();	// Create string for 24 hour time.
+		var object = {"time":time}			// These lines compile the Handlebars template for the time. 
+		var source = $("#timeTemplate").html(); 	// These four lines compile and apply the Handlebars template.
+		var template = Handlebars.compile(source);	// Compile template data into a js function.
+		var context = template(object);
+		var renderedText = $(context);
+		$("#time").empty();  
+		$("#time").append(renderedText);		// Add the rendered text to the html.
+		if (data == "unsuccessful_upload") {		// Update the last upload status text within the html. 
+			$("#message").empty();			
+			$("#message").append("<p>Your last file upload failed because there is already an existing file with the same name.</p>");		
+			$("#message").append();				
+		}
+		else {
+			$("#message").empty();			
+			$("#message").append("<p>Your last file was uploaded successfully. </p>");	
+		}		
+		var promise = $.get("/filenames");		// After the drop update the file list.
+		promise.then(addEntry);
+	};
+	
+	var uploadFailure = function(data) {			
+	}; 						
 
-	uploadSuccess(false); // Called to ensure we have initial data; 
+	var addEntry = function(data) {				// This function compiles and renders the Handlebars template for the table of filenames.
+		var source = $("#tableTemplate").html(); 
+		var template = Handlebars.compile(source);	// Compile template data into a js function.
+		var context = template(data);
+		var renderedText = $(context);
+		$("#tableArea").empty();  
+		$("#tableArea").append(renderedText);		// Add the rendered table to the html.
+	};
+
+	$(".dropzone").on(dropHandlerSet); 			// Attach event handlers to events occuring on .dropzone
+	uploadSuccess(false); 					// Called to ensure we have initial data; 
 });		
