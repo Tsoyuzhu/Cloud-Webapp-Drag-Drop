d9ec4bb264fb781c3af05d4bb9952c7c1d50b652
Tsoyuzhu <z5113384@student.unsw.edu.au>
2017-09-10 18:37:18 +1000

    updated bash scripts to check whether the required programs exist

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-echo "[Making sure uploads folder exists in app folder]"
+echo "Making sure uploads folder exists in app folder"
 cd ..
 mkdir uploads
 cd bin
-echo "[installing virtualenv]" 
+
+echo "Checking if pip is installed"
+echo "..."
+if hash pip 2>/dev/null; 
+then
+	echo "[pip is installed]"
+	pip install virtualenv
+else
+	echo "I require pip but it's not installed. Aborting."
+	exit 1
+fi
+
+echo "installing virtualenv using pip..."
+echo "..." 
 pip install virtualenv 
-echo "[creating virtualenv in current directory]"
+echo "creating virtualenv in current directory"
 virtualenv cloudAppProjectDependencies
 echo "[activating virtualenv]"
 source cloudAppProjectDependencies/bin/activate
-echo "[Installing flask...]" 
+echo "Installing flask..." 
 pip install flask
 deactivate
 echo "[Installations complete. Please execute 'run.sh']"
--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-echo "[initialising virtualenv...]"
+
+echo "Checking if python is installed..."
+echo "..."
+if hash python 2>/dev/null; 
+then
+	echo "[python is installed]"
+else
+	echo "I require python but it's not installed. Aborting."
+	exit 1
+fi
+echo "initialising virtualenv..."
 source cloudAppProjectDependencies/bin/activate
-echo "[moving you to app directory]"
+echo "moving you to app directory"
 cd ..
-python3 run.py 
+python run.py 
 echo 
 deactivate
-echo "[moving you back to bin directory]"
+echo "moving you back to bin directory"
 cd bin
-echo "[execution finished.]"
+echo "[execution of run.sh is finished]"
